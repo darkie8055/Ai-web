74afe464bda1979c727af69b761f27f8f95ecbf3
darkie8055 <76938388+darkie8055@users.noreply.github.com>
2025-08-12 01:12:17 +0530

    basic change
    Co-Authored-By: test-00-8 <149193225+test-00-8@users.noreply.github.com>

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,20 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
 export const appRouter = createTRPCRouter({
+  invoke: baseProcedure
+    .input(
+      z.object({
+        email: z.string(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      await inngest.send({
+        name: 'test/hello.world', 
+        email: input.email,
+      });
+      
+    }),
+
   createAI: baseProcedure
     .input(
       z.object({
